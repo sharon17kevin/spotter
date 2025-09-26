7cd7c924b865e19b4018739167dc333aec7ff604
sharon17kevin <sharon17kevin@gmail.com>
2025-09-26 14:28:09 +0100

    Create the api-client with axios

---
+++ frontend/src/services/api-client.ts
@@ -0,0 +1,34 @@
+import axios, { type AxiosRequestConfig } from "axios";
+
+export interface FetchResponse<T> {
+    count: number;
+    next: string | null;
+    results: T[];
+}
+
+const axiosInstance =  axios.create({
+    baseURL: 'https://api.rawg.io/api',
+    params: {
+        key: '7af71124eb32438eac14bfac6d2c8fa7'
+    }
+})
+
+export default class APIClient<T> {
+    endpoint: string;
+
+    constructor(endpoint: string) {
+        this.endpoint = endpoint
+    }
+
+    getAll = (config: AxiosRequestConfig) => {
+        return axiosInstance
+            .get<FetchResponse<T>>(this.endpoint, config)
+            .then(res=> res.data)
+    }
+
+    get = (id: string|number) => {
+        return axiosInstance
+        .get<T>(this.endpoint + '/' + id)
+        .then(res=>res.data)
+    }
+}
