60245b3805ecf2117442692ddfda2f13ce987ce0
sharon17kevin <sharon17kevin@gmail.com>
2025-09-28 12:47:04 +0100

    Change the baseURL on the api-client

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -7,7 +7,7 @@ export interface FetchResponse<T> {
 }
 
 const axiosInstance =  axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://spotter-2.onrender.com/api',
 })
 
 export default class APIClient<T> {
